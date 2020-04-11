10c74404da07e8d55a4288ecef628fc4f6481fa5
codando <joaolucasete.com@gmail.com>
2020-04-10 23:00:05 -0300

    Adiçao de mençao ao bot

--- preprocessing/parseMessage.js
+++ preprocessing/parseMessage.js
@@ -1,23 +1,29 @@
 module.exports = {
-    name: 'parseMessage',
-    priority: 1,
-    process: async container => {
-      const { msg, client, commands } = container;
-      if(!msg.channel.guild) return
-      const db = client.plugins.get("store");
-      const store = await db.fetchGuild(msg.channel.guild.id);
-      
-      const prefix = store.settings.prefix;
-      const language = store.settings.locale;
-  
-      if (!msg.content.startsWith(prefix)) return;
-  
-      const rawArgs = msg.content.substring(prefix.length).split(' ');
-      container.trigger = rawArgs[0].toLowerCase();
-      container.isCommand = commands.has(container.trigger);
-      container.rawArgs = rawArgs.slice(1).filter(v => v);
-      container.store = store;
-      container.settings = { lang: language, prefix }
-      return Promise.resolve(container);
+  name: 'parseMessage',
+  priority: 1,
+  process: async container => {
+    const { msg, client, commands } = container;
+    if (!msg.channel.guild) return
+    const db = client.plugins.get("store");
+    const store = await db.fetchGuild(msg.channel.guild.id);
+
+    const prefix = store.settings.prefix;
+    const language = store.settings.locale;
+
+    if (msg.content.startsWith(`<@!${client.user.id}>`)) {
+      const i18n = client.plugins.get("i18n")
+      const trad = i18n.parse("{{botMention}}", "commands", language, { prefix })
+      return msg.channel.createMessage(trad)
     }
+
+    else if (!msg.content.startsWith(prefix)) return;
+
+    const rawArgs = msg.content.substring(prefix.length).split(' ');
+    container.trigger = rawArgs[0].toLowerCase();
+    container.isCommand = commands.has(container.trigger);
+    container.rawArgs = rawArgs.slice(1).filter(v => v);
+    container.store = store;
+    container.settings = { lang: language, prefix }
+    return Promise.resolve(container);
   }
+}
